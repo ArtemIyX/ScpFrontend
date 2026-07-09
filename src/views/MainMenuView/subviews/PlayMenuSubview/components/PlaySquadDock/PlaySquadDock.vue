ec020a7b7644fd834416fc91c7b63da679ef24c3
Wellsaik <artem.podorozhko@outlook.com>
2026-07-09 23:51:18 +0300

    Compact view on lobby

--- src/views/MainMenuView/subviews/PlayMenuSubview/components/PlaySquadDock/PlaySquadDock.vue
+++ src/views/MainMenuView/subviews/PlayMenuSubview/components/PlaySquadDock/PlaySquadDock.vue
@@ -2,30 +2,32 @@
 
 <template>
   <section class="play-squad-dock" aria-label="Squad lobby slots">
-    <div class="play-squad-dock__invite-row">
+    <div class="play-squad-dock__row">
       <button type="button" class="play-squad-dock__invite-button">
         <span class="play-squad-dock__invite-icon" aria-hidden="true"></span>
         Invite players
       </button>
-    </div>
 
-    <div class="play-squad-dock__slots">
-      <article
-        v-for="slot in squadSlots"
-        :key="slot.id"
-        class="play-squad-dock__slot"
-        :class="{ 'play-squad-dock__slot--filled': slot.filled }"
-      >
-        <div class="play-squad-dock__avatar" :class="{ 'play-squad-dock__avatar--filled': slot.filled }">
-          <span v-if="slot.filled" class="play-squad-dock__avatar-text">O17</span>
-          <span v-else class="play-squad-dock__avatar-plus" aria-hidden="true">+</span>
-        </div>
+      <div class="play-squad-dock__slots">
+        <button
+          v-for="slot in squadSlots"
+          :key="slot.id"
+          type="button"
+          class="play-squad-dock__slot"
+          :class="{ 'play-squad-dock__slot--filled': slot.filled }"
+          :aria-label="slot.name"
+        >
+          <div class="play-squad-dock__avatar" :class="{ 'play-squad-dock__avatar--filled': slot.filled }">
+            <span v-if="slot.filled" class="play-squad-dock__avatar-text">O17</span>
+            <span v-else class="play-squad-dock__avatar-plus" aria-hidden="true">+</span>
+          </div>
 
-        <div class="play-squad-dock__meta">
-          <span class="play-squad-dock__label">{{ slot.label }}</span>
-          <span class="play-squad-dock__name">{{ slot.name }}</span>
-        </div>
-      </article>
+          <div class="play-squad-dock__tooltip" role="tooltip">
+            <span class="play-squad-dock__tooltip-label">{{ slot.label }}</span>
+            <span class="play-squad-dock__tooltip-name">{{ slot.name }}</span>
+          </div>
+        </button>
+      </div>
     </div>
   </section>
 </template>
