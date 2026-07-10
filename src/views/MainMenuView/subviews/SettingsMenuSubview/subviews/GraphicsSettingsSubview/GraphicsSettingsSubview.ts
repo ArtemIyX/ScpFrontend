49665939edce08ca797e33f91b290a60378189a0
Wellsaik <artem.podorozhko@outlook.com>
2026-07-10 11:33:08 +0300

    More normal names

--- src/views/MainMenuView/subviews/SettingsMenuSubview/subviews/GraphicsSettingsSubview/GraphicsSettingsSubview.ts
+++ src/views/MainMenuView/subviews/SettingsMenuSubview/subviews/GraphicsSettingsSubview/GraphicsSettingsSubview.ts
@@ -2,29 +2,34 @@ import { defineComponent, ref } from 'vue'
 
 import type { GRailItem } from '@/components/g/GRail/GRail'
 
-type QualityValue = 'low' | 'medium' | 'high' | 'epic'
+type QualityValue = 'low' | 'medium' | 'high' | 'epic' | 'cinematic'
 type MaterialQualityValue = 'low' | 'high'
 
 const scalabilityItems: GRailItem[] = [
   {
     value: 'low',
-    title: 'Low',
-    meta: 'Tier 0',
+    title: 'Very Low',
+    meta: 'Performance',
   },
   {
     value: 'medium',
-    title: 'Medium',
-    meta: 'Tier 1',
+    title: 'Low',
+    meta: 'Balanced',
   },
   {
     value: 'high',
-    title: 'High',
-    meta: 'Tier 2',
+    title: 'Medium',
+    meta: 'Detailed',
   },
   {
     value: 'epic',
-    title: 'Epic',
-    meta: 'Tier 3',
+    title: 'High',
+    meta: 'Ultra',
+  },
+  {
+    value: 'cinematic',
+    title: 'Ultra',
+    meta: 'Showcase',
   },
 ] as const
 
